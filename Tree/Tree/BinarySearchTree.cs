0e23dce3ed895a503fd4bbdea23f6b9a534a7300
on14an <112744129+On14an2@users.noreply.github.com>
2023-06-08 12:04:31 +0700

    Add files via upload

---
+++ Tree/Tree/BinarySearchTree.cs
@@ -0,0 +1,103 @@
+﻿public class BinarySearchTree
+{
+    private Node root;
+
+    public BinarySearchTree()
+    {
+        root = null;
+    }
+
+    public void Add(int value)
+    {
+        root = AddRecursive(root, value);
+    }
+
+    private Node AddRecursive(Node current, int value)
+    {
+        if (current == null)
+        {
+            return new Node(value);
+        }
+
+        if (value < current.Value)
+        {
+            current.Left = AddRecursive(current.Left, value);
+        }
+        else if (value > current.Value)
+        {
+            current.Right = AddRecursive(current.Right, value);
+        }
+
+        return current;
+    }
+
+    public void InOrderTraversal()
+    {
+        InOrderTraversalRecursive(root);
+        Console.WriteLine();
+    }
+
+    private void InOrderTraversalRecursive(Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        InOrderTraversalRecursive(node.Left);
+        Console.Write(node.Value + " ");
+        InOrderTraversalRecursive(node.Right);
+    }
+
+    public void PreOrderTraversal()
+    {
+        PreOrderTraversalRecursive(root);
+        Console.WriteLine();
+    }
+
+    private void PreOrderTraversalRecursive(Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        Console.Write(node.Value + " ");
+        PreOrderTraversalRecursive(node.Left);
+        PreOrderTraversalRecursive(node.Right);
+    }
+
+    public void PostOrderTraversal()
+    {
+        PostOrderTraversalRecursive(root);
+        Console.WriteLine();
+    }
+
+    private void PostOrderTraversalRecursive(Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        PostOrderTraversalRecursive(node.Left);
+        PostOrderTraversalRecursive(node.Right);
+        Console.Write(node.Value + " ");
+    }
+    public void PrintTree()
+    {
+        PrintTreeRecursive(root, 0);
+    }
+
+    private void PrintTreeRecursive(Node node, int level)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        PrintTreeRecursive(node.Right, level + 1);
+        Console.WriteLine(new string(' ', level * 4) + node.Value);
+        PrintTreeRecursive(node.Left, level + 1);
+    }
+}
---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.Add(4);
+        bst.Add(7);
+        bst.Add(3);
+        bst.Add(5);
+        bst.Add(2);
+        bst.Add(8);
+        bst.Add(1);
+
+        Console.WriteLine("Tree Visualization:");
+        bst.PrintTree();
+        
+        Console.WriteLine("In-Order Traversal:");
+        bst.InOrderTraversal();
+
+        Console.WriteLine("Pre-Order Traversal:");
+        bst.PreOrderTraversal();
+
+        Console.WriteLine("Post-Order Traversal:");
+        bst.PostOrderTraversal();
+    }
+}
